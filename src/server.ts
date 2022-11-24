61a1bc41b93c73d914da905a167b2612c304210f
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-11-24 20:53:34 -0300

    feat(endpoint): added the endpoint methods, some bugs to resolve

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,21 @@
 import express, { json } from 'express';
+import { v4, V4Options } from 'uuid';
 
-/*  */
+/* Import Express */
 const app = express();
 app.use(express.json());
 
 interface UserProps {
-  id: number;
+  id?: string;
   name: string;
   age: number | string;
 };
 
 /* Data */
 const users: UserProps[] = [
-  {id: 1, name: 'Adriano', age: 47},
-  {id: 2, name: 'Cristian', age: 14},
-  {id: 3, name: 'Grace', age: 18}
+  {name: 'Adriano', age: 47},
+  {name: 'Cristian', age: 14},
+  {name: 'Grace', age: 18}
 ];
 
 /* Get Method */
@@ -25,29 +26,46 @@ app.get('/users', (request, response) => {
 /* Post Method */
 app.post('/users', (request, response) => {
   const body = request.body;
+  const { name, age } = request.body;
   
-  console.log(body)
-  return response.json(body);
+  users.push({
+    id: v4(),
+    name,
+    age,
+  });
+
+  console.log(body);
+  return response.json(users);
 });
 
 /* Put Method */
 app.put('/users:id', (request, response) => {
   const { id } = request.params;
-  const body = request.body;
+  const { name, age } = request.body;
 
-  console.log(id)
+  const user = users.find(user => user.id === id);
+
+  if(!user) {
+    return response.status(400).json({error: 'User does not exists!'});
+  };
+
+  user.age = age;
+  user.name = name; 
+
+  console.log(id);
+
   return response.json(id);
-
-  console.log(body);
-  return response.json(body);
 });
 
 /* Delete Method */
-app.put('/users:id', (request, response) => {
+app.delete('/users', (request, response) => {
   const { id } = request.params;
 
-  console.log(id)
-  return response.json(id)
+  const user = users.find(user => user.id === id);
+
+  users.splice(user, 1);
+
+  return response.json(id);
 });
 
 /* Run Server */
